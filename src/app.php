83687c29bb8c20b2bf7ab64c5a94d71d3d7664a0
Ippei Sumida <ippey.s@gmail.com>
2021-04-02 18:50:10 +0900

    is_leap_year（閏年かチェック）ページを追加

--- src/app.php
+++ src/app.php
@@ -1,8 +1,18 @@
 <?php
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
+
+function is_leap_year($year = null)
+{
+    if (null === $year) {
+        $year = date('Y');
+    }
+
+    return 0 === $year % 400 || (0 === $year % 4 && 0 !== $year % 100);
+}
 
 $routes = new RouteCollection();
 $routes->add('hello', new Route('/hello/{name}', [
@@ -16,5 +26,16 @@ $routes->add('hello', new Route('/hello/{name}', [
     },
 ]));
 $routes->add('bye', new Route('/bye'));
+
+$routes->add('leap_year', new Route('/is_leap_year/{year}', [
+    'year' => null,
+    '_controller' => function (Request $request) {
+        if (is_leap_year($request->attributes->get('year'))) {
+            return new Response('Yep, this is a leap year.');
+        }
+
+        return new Response('Nope, this is not a leap year.');
+    },
+]));
 
 return $routes;
