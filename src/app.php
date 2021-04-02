4ecffb698842e420ca693ff40b5c59f7b071871c
Ippei Sumida <ippey.s@gmail.com>
2021-04-02 18:33:56 +0900

    reformat

--- src/app.php
+++ src/app.php
@@ -1,12 +1,13 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
 
 $routes = new RouteCollection();
 $routes->add('hello', new Route('/hello/{name}', [
     'name' => 'World',
-    '_controller' => function (\Symfony\Component\HttpFoundation\Request $request) {
+    '_controller' => function (Request $request) {
         $request->attributes->set('foo', 'bar');
 
         $response = render_template($request);
