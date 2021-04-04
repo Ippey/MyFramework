5af922aa5cf3b0c1ab5748162449556a787a6993
Ippei Sumida <ippey.s@gmail.com>
2021-04-04 18:34:51 +0900

    各パラメータの型をInterfaceに変更

--- src/Splash/Framework.php
+++ src/Splash/Framework.php
@@ -5,21 +5,21 @@ namespace Splash;
 use Exception;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
-use Symfony\Component\HttpKernel\Controller\ControllerResolver;
+use Symfony\Component\HttpKernel\Controller\ArgumentResolverInterface;
+use Symfony\Component\HttpKernel\Controller\ControllerResolverInterface;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
-use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\Matcher\UrlMatcherInterface;
 
 class Framework
 {
-    /** @var UrlMatcher  */
-    private UrlMatcher $matcher;
-    /** @var ControllerResolver  */
-    private ControllerResolver $controllerResolver;
-    /** @var ArgumentResolver  */
-    private ArgumentResolver $argumentResolver;
+    /** @var UrlMatcherInterface  */
+    private UrlMatcherInterface $matcher;
+    /** @var ControllerResolverInterface  */
+    private ControllerResolverInterface $controllerResolver;
+    /** @var ArgumentResolverInterface  */
+    private ArgumentResolverInterface $argumentResolver;
 
-    public function __construct(UrlMatcher $matcher, ControllerResolver $controllerResolver, ArgumentResolver $argumentResolver)
+    public function __construct(UrlMatcherInterface $matcher, ControllerResolverInterface $controllerResolver, ArgumentResolverInterface $argumentResolver)
     {
         $this->matcher = $matcher;
         $this->controllerResolver = $controllerResolver;
