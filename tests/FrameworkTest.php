6e3bb8c1448fc89a788233488aad023183d49a42
Ippei Sumida <ippey.s@gmail.com>
2021-04-04 18:34:31 +0900

    FrameworkTest追加

---
+++ tests/FrameworkTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Splash\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Splash\Framework;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Controller\ArgumentResolverInterface;
+use Symfony\Component\HttpKernel\Controller\ControllerResolverInterface;
+use Symfony\Component\Routing;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+
+class FrameworkTest extends TestCase
+{
+    public function testNotFoundHandling()
+    {
+        $framework = $this->getFrameworkForException(new ResourceNotFoundException());
+
+        $response = $framework->handle(new Request());
+
+        $this->assertEquals(404, $response->getStatusCode());
+    }
+
+    private function getFrameworkForException($exception)
+    {
+        /** @var Routing\Matcher\UrlMatcherInterface $matcher */
+        $matcher = $this->createMock(Routing\Matcher\UrlMatcherInterface::class);
+        // use getMock() on PHPUnit 5.3 or below
+        // $matcher = $this->getMock(Routing\Matcher\UrlMatcherInterface::class);
+
+        $matcher
+            ->expects($this->once())
+            ->method('match')
+            ->will($this->throwException($exception))
+        ;
+        $matcher
+            ->expects($this->once())
+            ->method('getContext')
+            ->will($this->returnValue($this->createMock(Routing\RequestContext::class)))
+        ;
+        /** @var ControllerResolverInterface $controllerResolver */
+        $controllerResolver = $this->createMock(ControllerResolverInterface::class);
+        /** @var ArgumentResolverInterface $argumentResolver */
+        $argumentResolver = $this->createMock(ArgumentResolverInterface::class);
+
+        return new Framework($matcher, $controllerResolver, $argumentResolver);
+    }
+}
