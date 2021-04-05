5f17dd2001c068ea2311d3acad6563ebe5acf968
Ippei Sumida <ippey.s@gmail.com>
2021-04-05 19:27:30 +0900

    FrameworkをHttpKernelの子クラスに変更

--- public/index.php
+++ public/index.php
@@ -16,19 +16,21 @@ use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\Routing\RequestContext;
 
 $request = Request::createFromGlobals();
+$requestStack = new \Symfony\Component\HttpFoundation\RequestStack();
 $routes = include __DIR__ . '/../src/app.php';
 
 $context = new RequestContext();
 $matcher = new UrlMatcher($routes, $context);
 
 $eventDispatcher = new EventDispatcher();
+$eventDispatcher->addSubscriber(new \Symfony\Component\HttpKernel\EventListener\RouterListener($matcher, $requestStack));
 $eventDispatcher->addSubscriber(new GoogleListener());
 $eventDispatcher->addSubscriber(new ContentLengthListener());
 
 $controllerResolver = new ControllerResolver();
 $argumentResolver = new ArgumentResolver();
 
-$framework = new Framework($eventDispatcher, $matcher, $controllerResolver, $argumentResolver);
+$framework = new Framework($eventDispatcher, $controllerResolver, $requestStack, $argumentResolver);
 $framework = new HttpCache(
     $framework,
     new Store(__DIR__ . '/../cache'),
