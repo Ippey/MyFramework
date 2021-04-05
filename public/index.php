1caac75c680790958bb268e461596e689a3dec6f
Ippei Sumida <ippey.s@gmail.com>
2021-04-05 19:38:51 +0900

    ErrorListener追加

--- public/index.php
+++ public/index.php
@@ -5,10 +5,14 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use Splash\EventListener\ContentLengthListener;
 use Splash\EventListener\GoogleListener;
 use Splash\Framework;
+use Symfony\Component\ErrorHandler\Exception\FlattenException;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
 use Symfony\Component\HttpKernel\Controller\ControllerResolver;
+use Symfony\Component\HttpKernel\EventListener\ErrorListener;
+use Symfony\Component\HttpKernel\EventListener\RouterListener;
 use Symfony\Component\HttpKernel\HttpCache\Esi;
 use Symfony\Component\HttpKernel\HttpCache\HttpCache;
 use Symfony\Component\HttpKernel\HttpCache\Store;
@@ -23,9 +27,15 @@ $context = new RequestContext();
 $matcher = new UrlMatcher($routes, $context);
 
 $eventDispatcher = new EventDispatcher();
-$eventDispatcher->addSubscriber(new \Symfony\Component\HttpKernel\EventListener\RouterListener($matcher, $requestStack));
+$eventDispatcher->addSubscriber(new RouterListener($matcher, $requestStack));
 $eventDispatcher->addSubscriber(new GoogleListener());
 $eventDispatcher->addSubscriber(new ContentLengthListener());
+$errorHandler = function (FlattenException $e) {
+    $message = 'Something went wrong. (' . $e->getMessage() . ')';
+
+    return new Response($message, $e->getStatusCode());
+};
+$eventDispatcher->addSubscriber(new ErrorListener($errorHandler));
 
 $controllerResolver = new ControllerResolver();
 $argumentResolver = new ArgumentResolver();
