46abb2ec9af48e8e839c2a1b40fe972663ad0bcf
Ippei Sumida <ippey.s@gmail.com>
2021-04-05 19:27:50 +0900

    reformat

--- src/Splash/Framework.php
+++ src/Splash/Framework.php
@@ -2,17 +2,7 @@
 
 namespace Splash;
 
-use Exception;
-use Splash\Event\ResponseEvent;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Controller\ArgumentResolverInterface;
-use Symfony\Component\HttpKernel\Controller\ControllerResolverInterface;
 use Symfony\Component\HttpKernel\HttpKernel;
-use Symfony\Component\HttpKernel\HttpKernelInterface;
-use Symfony\Component\Routing\Exception\ResourceNotFoundException;
-use Symfony\Component\Routing\Matcher\UrlMatcherInterface;
 
 class Framework extends HttpKernel
 {
